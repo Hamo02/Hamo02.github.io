99b7a60af2781172c8d7e8d220c27df9f56db881
hamo <ham.manucharyan@gmail.com>
2019-01-27 00:13:19 +0400

    13

--- restindex/Js/index.js
+++ restindex/Js/index.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
             var imagePos = $(this).offset().top;
             var topOfWindow = $(window).scrollTop();
             if (imagePos < topOfWindow + 500) {
-                $(this).addClass('flash');
+                $(this).addClass('bounce');
 
             }
 
